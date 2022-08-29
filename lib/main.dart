0d8b5a305983740705b1eda7dac27e94af029338
Yeasir Arefin Tusher <yasirarefinxz@gmail.com>
2022-08-29 15:40:36 +0600

    Boom: First run done successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_auth_with_riverpod_tutorial/core/presentation/widgets/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(AppWidget());
+  runApp(ProviderScope(child: AppWidget()));
 }
