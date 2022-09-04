a809371341ba5668853700a47652db80116853fb
Yeasir Arefin Tusher <yasirarefinxz@gmail.com>
2022-09-04 17:12:07 +0600

    Fix: comma in appwidget file

--- lib/core/presentation/widgets/app_widget.dart
+++ lib/core/presentation/widgets/app_widget.dart
@@ -17,36 +17,39 @@ class AppWidget extends ConsumerWidget {
     //listening to suthstate searching if user is authenticated or not
     /************************************** */
 
-    ref.listen<AuthState>(authNotifierProvider, (_, state) {
-      state.maybeWhen(
+    ref.listen<AuthState>(
+      authNotifierProvider,
+      (_, state) {
+        state.maybeWhen(
 //
-        // if user authenticated then
-        //
-        authenticated: () async {
-          final isEmailVerified = await ref
-              .read<EmailVerificationNotifier>(
-                  emailVerificationNotifierProvider.notifier)
-              .isEmailVerified();
-          //if users email not verified, go to Email Verification Page
-          //if users email verified, go to Home Page
+          // if user authenticated then
           //
-          if (!isEmailVerified == true) {
-            _appRouter.pushAndPopUntil(const EmailVerificationRoute(),
-                predicate: (_) => false);
-          } else {
-            _appRouter.pushAndPopUntil(const HomeRoute(),
-                predicate: (_) => false);
-          }
-        },
+          authenticated: () async {
+            final isEmailVerified = await ref
+                .read<EmailVerificationNotifier>(
+                    emailVerificationNotifierProvider.notifier)
+                .isEmailVerified();
+            //if users email not verified, go to Email Verification Page
+            //if users email verified, go to Home Page
+            //
+            if (!isEmailVerified == true) {
+              _appRouter.pushAndPopUntil(const EmailVerificationRoute(),
+                  predicate: (_) => false);
+            } else {
+              _appRouter.pushAndPopUntil(const HomeRoute(),
+                  predicate: (_) => false);
+            }
+          },
 
-        //if user unAuthenticated, go to SignIn Route
-        unAuthenticated: () {
-          _appRouter.pushAndPopUntil(const SignInRoute(),
-              predicate: (_) => false);
-        },
-        orElse: () {},
-      );
-    });
+          //if user unAuthenticated, go to SignIn Route
+          unAuthenticated: () {
+            _appRouter.pushAndPopUntil(const SignInRoute(),
+                predicate: (_) => false);
+          },
+          orElse: () {},
+        );
+      },
+    );
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
